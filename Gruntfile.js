19ce8ceb3ea307d26e3ddc89c99b1494caccf073
Emily <emilyrkarp@gmail.com>
2015-12-14 20:11:39 -0500

    trimmed Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,26 @@
 module.exports = function(grunt) {
 //require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
 
-	function ftpTask(files) {
-		return {
-			options: {
-					host: "wdsclient.com",
-					dest: "/public_html/em/log/",
-					authKey: "server",
-			},
-			files: [{
-				expand: true,
-				cwd: '.',
-				src: [ files ]
-			}]
-		}
-	};
-
-
 	grunt.config.init({
-		sass: {
-			options: {
-				sourceMap: true
-			},
-			dist: {
-				files: {
-					'stylesheets/style.css': 'sass/style.scss'
-				}
-			}
-		},
 		ftp_push: {
-			all: ftpTask([
-				"**.html",
-				"stylesheets/style.css",
-				"js/**"
-			]),
-			html: ftpTask([
-				'**.html'
-			]),
-			js: ftpTask([
-				'js/<%= path %>/**'
-			])
+			target: {
+				options: {
+						host: "wdsclient.com",
+						dest: "/public_html/em/sighs/",
+						authKey: "server",
+				},
+				files: [{
+					expand: true,
+					cwd: '.',
+					src: [
+						'index.html',
+						'sighs.txt'
+					]
+				}]
+			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-ftp-push');
-
-	grunt.registerTask('push', ['ftp_push:all']);
-	grunt.registerTask('html', ['ftp_push:html']);
-
-	grunt.registerTask('js', '', function(folder) {
-		var path = (folder == undefined) ? '**' : folder;
-		grunt.config.set('path', path)
-		grunt.task.run('ftp_push:js');
-	});
-
-	grunt.registerTask('default', ['sass', 'ftp_push:all']);
+	grunt.registerTask('push', ['ftp_push']);
 };
---
+++ README.md
@@ -0,0 +1,8 @@
+Add the following code to .bash_profile:
+
+`alias log='bash ~/Documents/web/log/script.sh'`
+
+
+Then to use in the terminal:
+
+$ log "Sample message"
